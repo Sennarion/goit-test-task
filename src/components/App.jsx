790a5c018acf174dd775f1dcf16086383c52c3a5
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2022-12-23 20:58:25 +0200

    add components and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import UsersList from './UsersList/UsersList';
+import initialUsers from '../data/users.json';
+import { Container } from './Container/Container.styled';
+import { GlobalStyles } from 'styles/GlobalStyles';
+
+const LS_KEY = 'users';
+
+export class App extends Component {
+  state = {
+    users: JSON.parse(localStorage.getItem(LS_KEY)) || initialUsers,
+  };
+
+  componentDidUpdate() {
+    localStorage.setItem(LS_KEY, JSON.stringify(this.state.users));
+  }
+
+  toggleFollow = id => {
+    this.setState(({ users }) => ({
+      users: users.map(user => {
+        if (user.id === id) {
+          return {
+            ...user,
+            followers: user.isFollow ? user.followers - 1 : user.followers + 1,
+            isFollow: user.isFollow ? false : true,
+          };
+        }
+        return user;
+      }),
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <Container>
+          <UsersList
+            users={this.state.users}
+            toggleFollow={this.toggleFollow}
+          />
+        </Container>
+        <GlobalStyles />
+      </>
+    );
+  }
+}
